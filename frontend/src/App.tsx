9992f10644823fbd4d63698993103a6a8c2237de
Abhiraj kumar <kumarabhiraj05981@gmail.com>
2026-08-09 18:33:35 +0530

    Add BranchResources route to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,26 +9,74 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Admin from "./pages/Admin";
 import AIQuestionPaper from "./pages/AIQuestionPaper";
+import BranchResources from "./pages/BranchResources";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/pyq" element={<Pyq />} />
-        <Route path="/syllabus" element={<Syllabus />} />
-        <Route path="/ebooks" element={<Ebooks />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<Admin />} />
 
-        {/* AI Question Paper */}
+      <Routes>
+
+        {/* HOME */}
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        {/* EXISTING RESOURCE PAGES */}
+        <Route
+          path="/notes"
+          element={<Notes />}
+        />
+
+        <Route
+          path="/pyq"
+          element={<Pyq />}
+        />
+
+        <Route
+          path="/syllabus"
+          element={<Syllabus />}
+        />
+
+        <Route
+          path="/ebooks"
+          element={<Ebooks />}
+        />
+
+        {/* BRANCH RESOURCES */}
+        <Route
+          path="/branch-resources"
+          element={<BranchResources />}
+        />
+
+        {/* AUTH */}
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        {/* ADMIN */}
+        <Route
+          path="/admin"
+          element={<Admin />}
+        />
+
+        {/* AI QUESTION PAPER */}
         <Route
           path="/ai-question-paper"
-          element={<AIQuestionPaper />}
+          element={
+            <AIQuestionPaper />
+          }
         />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
